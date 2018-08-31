63b1b658d656aea5719e82967fef458536381c20
majun <majun@kedacom.com>
2018-08-31 09:39:32 +0800

    调试

--- user-server/src/main/java/com/example/spring/cloud/user/server/controller/UserController.java
+++ user-server/src/main/java/com/example/spring/cloud/user/server/controller/UserController.java
@@ -1,15 +1,18 @@
 package com.example.spring.cloud.user.server.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/user")
+@Slf4j
 public class UserController {
 
     @GetMapping
     private String getUser() {
+        log.info("getUser ...");
         return "userId: 1";
     }
 }
